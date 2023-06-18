f3bbb0978ac378feefcbbf4c8d4048723d61a5e8
discipvlvsaeternvs <discipvlvs@gmail.com>
2023-06-19 00:19:45 +0700

    Add registration page

---
+++ register-form.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>JoDer - Job Finder - Register</title>
+	<link href="assets/stylesheets/form.css" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<?php include("navbar.php"); ?>
+	<div class="container w-50 mt-5">
+		<?php if (isset($_GET['status'])) : ?>
+			<?php if ($_GET['status'] == 'userExists') : ?>
+				<h1 class="bg-danger">The user being registered already exists!</h1>
+			<?php elseif ($_GET['status'] == 'registFailed') : ?>
+				<h1 class="bg-danger">Registration failed due to technical problem!</h1>
+			<?php endif; ?>
+		<?php endif; ?>
+		<form action="register.php" method="post">
+			<label for="email">Email</label>
+			<input id="email" type="email" name="email" placeholder="user@mail.com" required>
+			<label for="name">Name</label>
+			<input id="name" type="text" name="name" placeholder="Firstname Lastname" required>
+			<label for="type">Type</label>
+			<select name="type" id="type" required>
+				<option value="0">Admin</option>
+				<option value="1">Jobseeker</option>
+				<option value="2">Employer</option>
+			</select>
+			<label for="password">Password</label>
+			<input type="password" name="password" id="password" onkeyup="validate_passwords()" required>
+			<label for="password-confirm">Confirm Password</label>
+			<input type="password" name="password-confirm" id="password-confirm" onkeyup="validate_passwords()" required>
+			<button id="submitBtn" type="submit">Register</button>
+		</form>
+		<span id="warning" class="mt-5 text-center"></span>
+		<p class="mt-5 text-center">Already have an account? <a href="login-form.php">Login</a></p>
+	</div>
+	<script>
+		document.querySelector('#submitBtn').style.display = "none";
+
+		function validate_passwords() {
+			let password = document.querySelector('#password').value;
+			let cPassword = document.querySelector('#password-confirm').value;
+			let warningSpan = document.querySelector('#warning');
+			let registerBtn = document.querySelector('#submitBtn');
+			if (password !== "") {
+				if (password !== cPassword) {
+					warningSpan.textContent = "Passwords don't match!";
+					warningSpan.style.display = "inline";
+					warningSpan.classList.remove('bg-success');
+					warningSpan.classList.add('bg-danger');
+					registerBtn.style.display = "none";
+				} else {
+					warningSpan.textContent = "Passwords match!"
+					warningSpan.style.display = "inline";
+					warningSpan.classList.remove('bg-danger');
+					warningSpan.classList.add('bg-success');
+					registerBtn.style.display = "inline";
+				}
+			} else {
+				warningSpan.textContent = "";
+				warningSpan.style.display = "none";
+				registerBtn.style.display = "none";
+			}
+		}
+	</script>
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,26 @@
+<?php
+include("database.php");
+
+$e = $_POST['email'];
+$n = $_POST['name'];
+$p = $_POST['password'];
+$t = $_POST['type'];
+
+$check_prior_registration_sql = "SELECT id FROM Users WHERE email = '$e'";
+$res = mysqli_query($conn, $check_prior_registration_sql);
+
+if ($res) {
+	if (mysqli_num_rows($res) > 0) {
+		header("Location: register-form.php?status=userExists");
+	} else {
+
+		$register_sql = "INSERT INTO Users (email, name, password, type) VALUES ('$e', '{$n}', MD5('{$p}'), {$t})";
+		$res = mysqli_query($conn, $register_sql);
+
+		if ($res) {
+			header("Location: login-form.php?status=registSuccessful");
+		} else {
+			header("Location: register-form.php?status=registFailed");
+		}
+	}
+}
