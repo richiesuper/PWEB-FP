8e6d905287870316b86ae7309a504d1b06b70784
discipvlvsaeternvs <discipvlvs@gmail.com>
2023-06-18 21:36:00 +0700

    Add bulk files

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>JoDer - Job Finder</title>
+	<link href="assets/stylesheets/style.css" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<?php include("navbar.php"); ?>
+	<div class="banner-img"></div>
+	<div class="banner-text w-50">
+		<h1 class="mt-5 p-5 text-center">Welcome to JoDer - The Best Job Finder Platform on the Net!</h1>
+		<p class="p-5 text-center">Make your dream job come true through our curated list of highly in-demand jobs on the market</p>
+	</div>
+</body>
+
+</html>
