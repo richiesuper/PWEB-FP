f3a3267887a067a3f3c6f1fdb1eda7c5364540eb
gmaaliki <maalikighifari@gmail.com>
2023-06-19 08:25:49 +0700

    feat: Placing login to top right

--- navbar.php
+++ navbar.php
@@ -4,17 +4,26 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="job-listing.php">Job Listing</a>
         </li>
+      </ul>
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link" href="<?= isset($_SESSION['email']) ? "logout.php" : "login-form.php" ?>"><?= isset($_SESSION['email']) ? "Logout" : "Login" ?></a>
         </li>
+        <?php
+        if (!isset($_SESSION['email'])) {
+            echo "<li class=\"nav-item\">";
+            echo "  <a class=\"nav-link\" href=\"register-form.php\">Register</a>";
+            echo "</li>";
+        }
+        ?>
       </ul>
       <?php if ($_SERVER['SCRIPT_NAME'] == "/job-listing.php") : ?>
         <form class="d-flex" role="search">
