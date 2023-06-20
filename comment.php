834090d60206869c654c4f7558b45cb88cd708ea
gmaaliki <maalikighifari@gmail.com>
2023-06-20 15:52:31 +0700

    feat: Ultimate commit

---
+++ comment.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+session_start();
+include("database.php");
+
+$comment = $_POST['content'];
+$offer_id = $_POST['offer_id'];
+$user_id = $_SESSION['id'];
+
+$sql = "INSERT INTO Comments (user_id, offer_id, content) VALUES ({$user_id}, {$offer_id}, '{$comment}')";
+$res = mysqli_query($conn, $sql);
+
+if ($res) {
+    header("Location: job-detail.php?offer_id={$offer_id}");
+} else {
+    die("Error while fetching query");
+}
--- job-detail.php
+++ job-detail.php
@@ -24,26 +24,45 @@
     include("database.php");
     if (isset($_GET['offer_id'])) {
         $id = $_GET['offer_id'];
-        $fetch_jobs = "SELECT Offers.banner_path, Offers.title, Offers.descr, Users.name, Offers.id FROM Offers INNER JOIN Users_Offers ON Offers.id=Users_Offers.offer_id INNER JOIN Users ON Users_Offers.user_id=Users.id WHERE Offers.id=$id GROUP BY Offers.id";
-        $fetch_employer = "SELECT * FROM Users WHERE id=$id";
+        $fetch_jobs = "SELECT Offers.banner_path, Offers.title, Offers.descr, Users.name, Users.email, Offers.id FROM Offers INNER JOIN Users_Offers ON Offers.id=Users_Offers.offer_id INNER JOIN Users ON Users_Offers.user_id=Users.id WHERE Offers.id=$id GROUP BY Offers.id";
+        $fetch_comment = "SELECT Users.name, Comments.content FROM Comments INNER JOIN Users ON Comments.user_id=Users.id INNER JOIN Offers ON Comments.offer_id=Offers.id WHERE Offers.id=$id";
+
 
         $res_jobs = mysqli_query($conn, $fetch_jobs);
-        $res_employer = mysqli_query($conn, $fetch_employer);
+        $res_comment = mysqli_query($conn, $fetch_comment);
+        $num_comment = mysqli_num_rows($res_comment);
+        
+        if(!$res_jobs) die("Error on query");
 
-        if($res_jobs) {
-            $row_jobs = mysqli_fetch_assoc($res_jobs);
-            echo "<div class=\"card m-3\">";
-            echo "    <div class=\"card-body\">";
-            echo "        <h5 class=\"card-title\">" . $row_jobs['title'] . "</h5>";
-            echo "        <img class=\"card-img-top\" src=\"" . $row_jobs['banner_path'] . "\" alt=\"Banner pekerjaan\">";
-            echo "        <br><br>";
-            echo "        <h6 class=\"card-subtitle mb-2 text-muted\">Ditawarkan oleh: " . $row_jobs['name'] . "</h6>";
-            echo "        <p class=\"card-text\">" . $row_jobs['descr'] . "</p>";
-            echo "    </div>";
+        $row_jobs = mysqli_fetch_assoc($res_jobs);
+        echo "<div class=\"card m-3 w-50 mx-auto\">";
+        echo "    <div class=\"card-body\">";
+        echo "        <h3 class=\"card-title\">" . $row_jobs['title'] . "</h3>";
+        echo "        <img class=\"card-img-top\" src=\"" . $row_jobs['banner_path'] . "\" alt=\"Banner pekerjaan\">";
+        echo "        <br><br>";
+        echo "        <h6 class=\"card-subtitle mb-2 text-muted\">Ditawarkan oleh: " . $row_jobs['name'] . "</h6>";
+        echo "        <p class=\"card-text\">" . $row_jobs['descr'] . "</p>";
+        echo "        <p class=\"card-text\">Email: " . $row_jobs['email'] . "</p>";
+        echo "        <div>";
+        echo "            <h5 class=\"card-title\">$num_comment Komentar</h5>";
+        echo "            <form class=\"my-2\" action=\"comment.php\" method=\"post\" enctype=\"multipart/form-data\">";
+        echo "                <img class=\"card-img-top mx-2\" src=\"/PWEB-FP/assets/img/profilephoto.jpg\" alt=\"Banner pekerjaan\" style=\"width: 30px; border-radius: 50%\">";
+        echo "                <input id=\"content\" type=\"text\" name=\"content\" placeholder=\"Add a comment\">";
+        echo "                <input name=\"offer_id\" value=\"{$id}\" style=\"display: none;\">";
+        echo "                <button id=\"submitBtn\" type=\"submit\" name=\"post\" class=\"btn btn-primary post-button\">Submit</button>";
+        echo "            </form>";
+        while($row_comment = mysqli_fetch_assoc($res_comment)) {
+            echo "<div>";
+            echo "    <img class=\"card-img-top mx-2\" src=\"/PWEB-FP/assets/img/profilephoto.jpg\" alt=\"Banner pekerjaan\" style=\"width: 30px; border-radius: 50%;\">";
+            echo "    <span>" . $row_comment['name'] . "</span>";
+            echo "    <p class=\"mx-5\">" . $row_comment['content'] . "</p>";
             echo "</div>";
-        } else {
-            die("Error on query");
         }
+        echo "            ";
+        echo "        </div>";
+        echo "    </div>";
+        echo "</div>";
+
     } else {
         die("Error on fetching variable");
     }
