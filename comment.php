07ab3c685acec581a08acee422cfa1ec8226bf76
discipvlvsaeternvs <discipvlvs@gmail.com>
2023-06-20 22:20:53 +0700

    Fix comment.php

--- comment.php
+++ comment.php
@@ -8,7 +8,7 @@ $comment = $_POST['content'];
 $offer_id = $_POST['offer_id'];
 $user_id = $_SESSION['id'];
 
-$sql = "INSERT INTO Comments (user_id, offer_id, content) VALUES ({$user_id}, {$offer_id}, `{$comment}`)";
+$sql = "INSERT INTO Comments (user_id, offer_id, content) VALUES ({$user_id}, {$offer_id}, \"{$comment}\")";
 $res = mysqli_query($conn, $sql);
 
 if ($res) {
