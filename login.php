4ef649e63131febcbfc546f431b835fcf75f7c18
discipvlvsaeternvs <discipvlvs@gmail.com>
2023-06-20 12:40:30 +0700

    Store user type and id in $_SESSION in login.php

--- login.php
+++ login.php
@@ -11,8 +11,12 @@ $res = mysqli_query($conn, $check_prior_registration_sql);
 if ($res) {
     $row = mysqli_fetch_assoc($res);
     $password = md5($p);
+    $userType = $row['type'];
+    $userId = $row['id'];
     if($password == $row['password']) {
         $_SESSION['email'] = $e;
+        $_SESSION['type'] = $userType;
+        $_SESSION['id'] = $userId;
 	} else {
        die("Error password or email doesn't match");
 	}
