404bf99a1d1674ef15adf63a0327046ee5d3c3fb
discipvlvsaeternvs <discipvlvs@gmail.com>
2023-06-20 22:32:08 +0700

    comment.php

--- comment.php
+++ comment.php
@@ -9,7 +9,7 @@ $offer_id = $_POST['offer_id'];
 $user_id = $_SESSION['id'];
 $escaped_comment = mysqli_real_escape_string($conn, $comment);
 
-$sql = "INSERT INTO Comments (user_id, offer_id, content) VALUES ({$user_id}, {$offer_id}, {$escaped_comment})";
+$sql = "INSERT INTO Comments (user_id, offer_id, content) VALUES ({$user_id}, {$offer_id}, '{$escaped_comment}')";
 $res = mysqli_query($conn, $sql);
 
 if ($res) {
