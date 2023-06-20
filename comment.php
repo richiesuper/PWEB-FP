9e6e4fd584ec5e6e63e1f4b0b4513416f2581a64
discipvlvsaeternvs <discipvlvs@gmail.com>
2023-06-20 22:31:39 +0700

    Fix comment.php

--- comment.php
+++ comment.php
@@ -7,7 +7,7 @@ include("database.php");
 $comment = $_POST['content'];
 $offer_id = $_POST['offer_id'];
 $user_id = $_SESSION['id'];
-$escaped_comment = mysqli_real_escape_string($comment, $conn);
+$escaped_comment = mysqli_real_escape_string($conn, $comment);
 
 $sql = "INSERT INTO Comments (user_id, offer_id, content) VALUES ({$user_id}, {$offer_id}, {$escaped_comment})";
 $res = mysqli_query($conn, $sql);
